dbe8f1a8e1edfd2c7e47c791496673038d0f4036
mninozebra <mni@nozebra.dk>
2019-01-11 16:07:10 +0100

    Adding new layout to site

--- mikkelraev.dk/src/App.vue
+++ mikkelraev.dk/src/App.vue
@@ -9,7 +9,7 @@
             <v-icon>dashboard</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title>Dashboard</v-list-tile-title>
+            <v-list-tile-title>Om</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
         <v-list-tile @click>
@@ -17,7 +17,34 @@
             <v-icon>settings</v-icon>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title>Settings</v-list-tile-title>
+            <v-list-tile-title>Kompetencer</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
+        <v-list-tile @click>
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Kompetencer</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
+        <v-list-tile @click>
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Kompetencer</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+
+        <v-list-tile @click>
+          <v-list-tile-action>
+            <v-icon>settings</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Kompetencer</v-list-tile-title>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
